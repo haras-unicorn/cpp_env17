2bd3d640ec43c586fc5c8ddab6964b8de0a33aa1
Hrvoje Juric <hrvoje.juric.13@gmail.com>
2021-10-27 04:50:23 +0200

    bench fix

--- bench/test.cpp
+++ bench/test.cpp
@@ -3,11 +3,11 @@
 
 void bench(benchmark::State& state)
 {
-    for ([[maybe_unused]] auto current : state)
-    {
-        std::vector<int> a{100};
-        benchmark::ClobberMemory();
-    }
+  for ([[maybe_unused]] auto current : state)
+  {
+    std::vector<int> a{100};
+    benchmark::ClobberMemory();
+  }
 }
 
-ENV_BENCH_CASE(bench)->Range(1, 10);
+BENCHMARK(bench)->Range(1, 10);
